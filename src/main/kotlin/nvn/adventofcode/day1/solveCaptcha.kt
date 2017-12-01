30b9ee7777d580a2da265bc04b1b464654bb7cba
nick.vannieuwenhuyse <nick.vannieuwenhuyse@axians.com>
2017-12-01 13:51:47 +0100

    Day 1: decrease line length

--- src/main/kotlin/nvn/adventofcode/day1/solveCaptcha.kt
+++ src/main/kotlin/nvn/adventofcode/day1/solveCaptcha.kt
@@ -2,7 +2,10 @@ package nvn.adventofcode.day1
 
 fun solveCaptchaPart2(input: String): Int = solveCaptcha(input, { it + (input.length / 2) })
 
-fun solveCaptcha(input: String, matchingStrategy: (Int) -> Int = { it + 1 }): Int = input.foldIndexed(0, { index, acc, char ->
+fun solveCaptcha(
+        input: String,
+        matchingStrategy: (Int) -> Int = { it + 1 }
+): Int = input.foldIndexed(0, { index, acc, char ->
     val matchIndex = matchingStrategy(index) % input.length
     val matchChar = input[matchIndex]
 
